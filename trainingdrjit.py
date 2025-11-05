520c4232f998263f9d5c733d223a4699c00dd13e
Rasmus Clausen <vez64ryj@gmail.com>
2025-11-05 12:32:07 +0100

    pushing from laptop

--- trainingdrjit.py
+++ trainingdrjit.py
@@ -13,29 +13,32 @@ import argparse
 import drjit.auto.ad as drad
 
 
-
-
+# dr.set_backend("scalar")
+# dr.set_flag(dr.JitFlag.Debug, True)
+# dr.set_flag(dr.JitFlag.Debug, True)          # Enables bounds checks & extra validation
+dr.set_flag(dr.JitFlag.SymbolicScope, False)          # Optional: verbose kernel trace
+dr.set_flag(dr.JitFlag.SymbolicCalls, False)
+dr.set_flag(dr.JitFlag.SymbolicConditionals, False)
+dr.set_flag(dr.JitFlag.SymbolicScope, False)
 def main():
     args = arguments_parsing.parse_args()
-    training_set, validation_set, _ = dataset_optimized.create_dataloaders_optimized(
-        path=args.data,
-        batch_size=10,
-        num_workers=args.num_workers,
-        pin_memory=True,
-        use_batch_sampler=True,
-    ) 
+    samples = dataset_optimized.load_sample_file(args.data)
+
+    # gather first 5000 samples for training
+    import numpy as np
+    # train_set = np.array([training_set.get for _ in range(1000)])
+    
+    print(f"Number of training samples: {len(samples['f'])}")
 
     inputsize = 10
 
     net = nn.Sequential(
-        nn.Cast(Float16),
         nn.Linear(inputsize, 64),
         nn.ReLU(),
         nn.Linear(64, 64),
         nn.ReLU(),
         nn.Linear(64, 3),
         nn.Exp(),
-        nn.Cast(Float32)
     )
 
     
@@ -43,13 +46,12 @@ def main():
 
 
     tranining_type = drad.TensorXf16
-
+    print("Using training type:", tranining_type)
     net = net.alloc(
         dtype=tranining_type,
         size=-1,
         rng=rng
     )
-
     weights, net = nn.pack(net, layout='training')
     dr.enable_grad(weights)
     print(net)
@@ -60,39 +62,34 @@ def main():
 
     res = 256
     
-    
+    batch_size = args.batch_size
+    loss_avg = drad.Float32(0.0)
+    counter = 0
 
-    for epoch in range(5):
-        for b in training_set:
-            wi_local = drad.TensorXf(b['wi_local'][0].T)
-            wo_local = drad.TensorXf(b['wo_local'][0].T)
-            # normal = dr.auto.TensorXf(b['shading_normal'])
-            albedo = drad.TensorXf(b['albedo'][0].T)
-            roughness = drad.TensorXf(b['roughness'][0].T)
-            target = drad.Array3f(b['f'][0].T)
-
-            input_tensor = drad.TensorXf16(dr.concat([
-                wi_local[:],
-                wo_local[:],
-                albedo,
-                roughness
-            ], axis=0))
-            # print(input_tensor.shape)
+    for epoch in tqdm(range(5), desc="Overall Training Progress", unit="epoch", total=5):
+        loss_avg = drad.Float32(0.0)
+        # for b in range(len(samples['f'])//batch_size):
+        for b in tqdm(range(len(samples['f'])//batch_size), desc=f"Epoch {epoch+1}", total=len(samples['f'])//batch_size, unit="batch"):
+            wi_local = samples['wi_local'][b*batch_size:(b+1)*batch_size]      # expect shape (3,)
+            wo_local = samples['wo_local'][b*batch_size:(b+1)*batch_size]      # (3,)
+            albedo   = samples['albedo'][b*batch_size:(b+1)*batch_size]        # (3,)
+            r_val    = samples['roughness'][b*batch_size:(b+1)*batch_size]
+            
+            r_val = np.expand_dims(r_val, axis=-1)  # (1,)
+            
+            features = np.concatenate([wi_local, wo_local, albedo, r_val], axis=-1).T
+            target = drad.TensorXf(samples['f'][b*batch_size:(b+1)*batch_size].T)
+            
+            input_tensor = drad.TensorXf16(features)
 
             input_tensor = dr.nn.CoopVec(input_tensor)
             pred = net(input_tensor)
-            pred = drad.Array3f(pred)
-            # print(f"pred: {pred}")
+            pred = drad.TensorXf(pred)
             loss = dr.mean(dr.square(pred - target))
-            # loss = dr.auto.Float16(loss)
-            # print("depends_on(weights)?", dr.depends_on(loss, weights))
-
-            # print(f"loss: {loss}")
-            
+            loss_avg += loss / (len(samples['f']) // batch_size)
             dr.backward(scaler.scale(loss))
             scaler.step(opt)
-            dr.set_grad(weights, 0)
-            
-    
+            dr.clear_grad(weights)
+        
 if __name__ == "__main__":
     main()
